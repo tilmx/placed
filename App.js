baad23c8d7c2ab7f2a0e93596d5dad7f38657e00
tilmx <github@tilmanfrick.com>
2018-01-08 00:22:59 +0100

    add experimental text

--- App.js
+++ App.js
@@ -2,22 +2,30 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.headline}>All your 37 items are stored safely in your <Text style={styles.selection}>studio</Text>.</Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		paddingTop: 180,
+		paddingLeft: 30,
+		paddingRight: 30,
+		backgroundColor: '#fff',
+	},
+	headline: {
+		fontFamily: 'System',
+		fontSize: 27,
+		lineHeight: 36,
+		fontWeight: '700',
+	},
+	selection: {
+		textDecorationLine: 'underline',
+		textDecorationColor: '#ff0000'
+	},
 });
